e6077120add99dc999732e9d33e3a3aa2be6d318
Dennis <nndenninn@gmail.com>
2020-04-08 10:49:26 +0300

    Refactoring and added extension for navigationBar

--- ToDoCoreData-iOS13/Controller/ToDoListViewController.swift
+++ ToDoCoreData-iOS13/Controller/ToDoListViewController.swift
@@ -11,16 +11,23 @@ import CoreData
 
 class ToDoListViewController: UITableViewController {
     var itemArr = [Item]()
+    var selectedCategory: Category? {
+        didSet {
+            loadItems()
+        }
+    }
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureNavigationBar(largeTitleColor: .white, backgoundColor: .blue, tintColor: .white, title: "tems", preferredLargeTitle: false)
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
-        
-        loadItems()
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
     // MARK: - tableViewDataSource
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -54,22 +61,24 @@ class ToDoListViewController: UITableViewController {
         
         var textField = UITextField()
         let alert = UIAlertController(title: "Add new item", message: nil, preferredStyle: .alert)
-        let action = UIAlertAction(title: "Add item", style: .default) { (action) in
+        let action = UIAlertAction(title: "Add item", style: .default) { _ in
             let newItem = Item(context: self.context)
             newItem.title = textField.text!
             newItem.done = false
+            newItem.parentCathegory = self.selectedCategory
             guard newItem.title != "" else { return }
             self.itemArr.append(newItem)
             
             self.saveItems()
         }
-        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
             
             textField = alertTextField
         }
         alert.addAction(action)
+        alert.addAction(cancelAction)
         present(alert, animated: true)
     }
     
@@ -80,7 +89,15 @@ class ToDoListViewController: UITableViewController {
         tableView.reloadData()
     }
     
-    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        let categoryPredicate = NSPredicate(format: "parentCathegory.name MATCHES %@", selectedCategory!.name!)
+        
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        } else {
+            request.predicate = categoryPredicate
+        }
+        
         do { itemArr = try context.fetch(request) }
         catch { print("Error fetching data from context, \(error)") }
         tableView.reloadData()
@@ -90,10 +107,10 @@ class ToDoListViewController: UITableViewController {
 extension ToDoListViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         let request: NSFetchRequest<Item> = Item.fetchRequest()
-        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
         
-        loadItems(with: request)
+        loadItems(with: request, predicate: predicate)
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
